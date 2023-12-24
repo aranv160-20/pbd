a674f8e574c92be738c0f02cfc79cbecc36ba8dd
ara <ameliaarahayu138@gmail.com>
2023-12-24 10:13:30 +0700

    Update curd_mahasiswa.php

--- ua/curd_mahasiswa.php
+++ ua/curd_mahasiswa.php
@@ -1,177 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi CRUD Mahasiswa Keren</title>
-
-    <style>
-       <style>
-    body {
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        background-color: #f8f9fa;
-        margin: 0;
-        padding: 0;
-    }
-
-    .container {
-        max-width: 800px;
-        margin: 20px auto;
-        padding: 20px;
-        background-color: #ffffff;
-        border-radius: 8px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    h2 {
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px;
-        border-radius: 5px 5px 0 0;
-        margin-top: 0;
-    }
-
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        overflow: hidden;
-    }
-
-    th, td {
-        border: 1px solid #ddd;
-        padding: 12px;
-        text-align: left;
-        transition: background-color 0.3s ease;
-    }
-
-    th {
-        background-color: #007bff;
-        color: #fff;
-        position: relative;
-    }
-
-    th:before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(90deg, #007bff, #0056b3);
-        opacity: 0.6;
-        z-index: -1;
-    }
-
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
-
-    tr:hover {
-        background-color: #e2e6ea;
-    }
-
-    .actions {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 10px;
-    }
-
-    .actions a {
-        text-decoration: none;
-        padding: 12px 20px;
-        border-radius: 5px;
-        font-size: 16px;
-        transition: background-color 0.3s ease;
-        cursor: pointer;
-    }
-
-    .actions a.input {
-        background-color: #007bff;
-        color: #fff;
-    }
-
-    .actions a.input:hover {
-        background-color: #0056b3;
-    }
-
-    .actions a.delete {
-        background-color: #dc3545;
-        color: #fff;
-    }
-
-    .actions a.delete:hover {
-        background-color: #c82333;
-    }
-
-    .actions a.update {
-        background-color: #28a745;
-        color: #fff;
-    }
-
-    .actions a.update:hover {
-        background-color: #218838;
-    }
-
-    .form-input {
-        margin-top: 20px;
-    }
-
-    .form-input label {
-        display: block;
-        margin-bottom: 5px;
-        color: #495057;
-    }
-
-    .form-input input[type="text"],
-    .form-input input[type="radio"] {
-        width: 100%;
-        padding: 10px;
-        margin-bottom: 10px;
-        border: 1px solid #ced4da;
-        border-radius: 5px;
-        font-size: 16px;
-    }
-
-    .form-input input[type="radio"] {
-        width: auto;
-        margin-right: 10px;
-    }
-
-    .form-input input[type="submit"] {
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        padding: 12px 20px;
-        cursor: pointer;
-        border-radius: 5px;
-        transition: background-color 0.3s ease;
-    }
-
-    .form-input input[type="submit"]:hover {
-        background-color: #0056b3;
-    }
-
-    .form-input a {
-        text-decoration: none;
-        margin-left: 10px;
-        color: #007bff;
-        transition: color 0.3s ease;
-    }
-
-    .form-input a:hover {
-        color: #0056b3;
-    }
-</style>
-
-    </style>
-
-</head>
-
-<body>
-    <div class="container">
-        <?php
+<?php
         session_start();
 
         // Check if the user is logged in and has the required permissions
@@ -226,6 +54,178 @@
             $query = "SELECT * FROM mahasiswa";
             $result = mysqli_query($hub, $query);
             ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi CRUD Mahasiswa Keren</title>
+
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px;
+            border-radius: 5px 5px 0 0;
+            margin-top: 0;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+            transition: background-color 0.3s ease;
+        }
+
+        th {
+            background-color: #007bff;
+            color: #fff;
+            position: relative;
+        }
+
+        th:before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: linear-gradient(90deg, #007bff, #0056b3);
+            opacity: 0.6;
+            z-index: -1;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #e2e6ea;
+        }
+
+        .actions {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 10px;
+        }
+
+        .actions a {
+            text-decoration: none;
+            padding: 12px 20px;
+            border-radius: 5px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+            cursor: pointer;
+        }
+
+        .actions a.input {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        .actions a.input:hover {
+            background-color: #0056b3;
+        }
+
+        .actions a.delete {
+            background-color: #dc3545;
+            color: #fff;
+        }
+
+        .actions a.delete:hover {
+            background-color: #c82333;
+        }
+
+        .actions a.update {
+            background-color: #28a745;
+            color: #fff;
+        }
+
+        .actions a.update:hover {
+            background-color: #218838;
+        }
+
+        .form-input {
+            margin-top: 20px;
+        }
+
+        .form-input label {
+            display: block;
+            margin-bottom: 5px;
+            color: #495057;
+        }
+
+        .form-input input[type="text"],
+        .form-input input[type="radio"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ced4da;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        .form-input input[type="radio"] {
+            width: auto;
+            margin-right: 10px;
+        }
+
+        .form-input input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            padding: 12px 20px;
+            cursor: pointer;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .form-input input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+
+        .form-input a {
+            text-decoration: none;
+            margin-left: 10px;
+            color: #007bff;
+            transition: color 0.3s ease;
+        }
+
+        .form-input a:hover {
+            color: #0056b3;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container">
             <h2>Read Data Mahasiswa</h2>
             <table border=1 cellpadding=2>
                 <tr>
@@ -294,7 +294,7 @@
         </form>
     <?php
     }
-    
+
     function edit_data($id)
     {
         global $hub;
@@ -328,74 +328,75 @@
         </form>
     <?php
     }
-    
 
     function hapus_data($id)
-        {
-            global $hub;
-            $query = "SELECT * FROM mahasiswa WHERE idmhs = $id";
-            $result = mysqli_query($hub, $query);
-            $row = mysqli_fetch_array($result);
-            ?>
-                <h2>Hapus Data Mahasiswa</h2>
-                <form action="curd_mahasiswa.php?a=list" method="post" class="form-input">
-                    <input type="hidden" name="sql" value="delete">
-                    <input type="hidden" name="idmhs" value="<?php echo htmlspecialchars($id); ?>">
-                    <table>
-                        <tr>
-                            <td width=100>NPM</td>
-                            <td><?php echo htmlspecialchars($row["npm"]); ?></td>
-                        </tr>
-                        <tr>
-                            <td>Nama Mahasiswa</td>
-                            <td><?php echo htmlspecialchars($row["nama"]); ?></td>
-                        </tr>
-                        <tr>
-                            <td>Program Studi</td>
-                            <td><?php echo htmlspecialchars($row["idprodi"]); ?></td>
-                        </tr>
-                    </table>
-                    <br><input type="submit" name="action" value="Hapus">
-                    <a href="curd_mahasiswa.php?a=list">Batal</a>
-                </form>
-            <?php
-        }
+    {
+        global $hub;
+        $query_prodi = "SELECT * FROM dt_prodi";
+        $result_prodi = mysqli_query($hub, $query_prodi);
+        $query = "SELECT * FROM mahasiswa WHERE idmhs = $id";
+        $result = mysqli_query($hub, $query);
+        $row = mysqli_fetch_array($result);
+        ?>
+        <h2>Hapus Data Mahasiswa</h2>
+        <form action="curd_mahasiswa.php?a=list" method="post" class="form-input">
+            <input type="hidden" name="sql" value="delete">
+            <input type="hidden" name="idmhs" value="<?php echo htmlspecialchars($id); ?>">
+            <table>
+                <tr>
+                    <td width=100>NPM</td>
+                    <td><?php echo htmlspecialchars($row["npm"]); ?></td>
+                </tr>
+                <tr>
+                    <td>Nama Mahasiswa</td>
+                    <td><?php echo htmlspecialchars($row["nama"]); ?></td>
+                </tr>
+                <tr>
+                    <td>Program Studi</td>
+                    <td><?php echo htmlspecialchars($row["idprodi"]); ?></td>
+                </tr>
+            </table>
+            <br><input type="submit" name="action" value="Hapus">
+            <a href="curd_mahasiswa.php?a=list">Batal</a>
+        </form>
+    <?php
+    }
 
-        function create_mahasiswa()
-        {
-            global $hub;
-            global $_POST;
-            $npm = mysqli_real_escape_string($hub, $_POST["npm"]);
-            $nama = mysqli_real_escape_string($hub, $_POST["nama"]);
-            $idprodi = mysqli_real_escape_string($hub, $_POST["idprodi"]);
+    function create_mahasiswa()
+    {
+        global $hub;
+        global $_POST;
+        $npm = mysqli_real_escape_string($hub, $_POST["npm"]);
+        $nama = mysqli_real_escape_string($hub, $_POST["nama"]);
+        $idprodi = mysqli_real_escape_string($hub, $_POST["idprodi"]);
 
-            $query  = "INSERT INTO `mahasiswa` (`npm`, `nama`, `idprodi`) VALUES ";
-            $query .= "('$npm', '$nama', '$idprodi')";
-            mysqli_query($hub, $query) or die(mysqli_error($hub));
-        }
+        $query  = "INSERT INTO `mahasiswa` (`npm`, `nama`, `idprodi`) VALUES ";
+        $query .= "('$npm', '$nama', '$idprodi')";
+        mysqli_query($hub, $query) or die(mysqli_error($hub));
+    }
 
-        function update_mahasiswa()
-        {
-            global $hub;
-            global $_POST;
-            $idmhs = mysqli_real_escape_string($hub, $_POST["idmhs"]);
-            $npm = mysqli_real_escape_string($hub, $_POST["npm"]);
-            $nama = mysqli_real_escape_string($hub, $_POST["nama"]);
-            $idprodi = mysqli_real_escape_string($hub, $_POST["idprodi"]);
+    function update_mahasiswa()
+    {
+        global $hub;
+        global $_POST;
+        $idmhs = mysqli_real_escape_string($hub, $_POST["idmhs"]);
+        $npm = mysqli_real_escape_string($hub, $_POST["npm"]);
+        $nama = mysqli_real_escape_string($hub, $_POST["nama"]);
+        $idprodi = mysqli_real_escape_string($hub, $_POST["idprodi"]);
 
-            $query  = "UPDATE `mahasiswa` SET npm='$npm', nama='$nama', idprodi='$idprodi' WHERE idmhs=$idmhs";
-            mysqli_query($hub, $query) or die(mysqli_error($hub));
-        }
+        $query  = "UPDATE `mahasiswa` SET npm='$npm', nama='$nama', idprodi='$idprodi' WHERE idmhs=$idmhs";
+        mysqli_query($hub, $query) or die(mysqli_error($hub));
+    }
 
-        function delete_mahasiswa()
-        {
-            global $hub;
-            global $_POST;
-            $idmhs = mysqli_real_escape_string($hub, $_POST["idmhs"]);
+    function delete_mahasiswa()
+    {
+        global $hub;
+        global $_POST;
+        $idmhs = mysqli_real_escape_string($hub, $_POST["idmhs"]);
 
-            $query  = "DELETE FROM `mahasiswa` WHERE idmhs=$idmhs";
-            mysqli_query($hub, $query) or die(mysqli_error($hub));
-        }
+        $query  = "DELETE FROM `mahasiswa` WHERE idmhs=$idmhs";
+        mysqli_query($hub, $query) or die(mysqli_error($hub));
+    }
     ?>
     </div>
 </body>
